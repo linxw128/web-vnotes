effaabe5117e273a7037a90905436168a0b6a67a
linxiaowei <linxw128@163.com>
2026-01-24 17:25:04 +0800

    WIP: 临时保存进度

--- build.sh
+++ build.sh
@@ -1,6 +1,7 @@
 echo "will auto remove dist, if not do this, the build process will occur out of memory bug"
 rm -fr dist
 pnpm run build
+echo "pack dist"
 tar -czf dist.tar.gz dist
 tar -czf webhook.tar.gz webhook
 git add .

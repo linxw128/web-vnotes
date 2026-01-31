b9fd3989169cf769c962ef5547f76b6672812dec
linxiaowei <linxw128@163.com>
2026-01-31 11:55:02 +0800

    Add baidu tongji js code

--- build.sh
+++ build.sh
@@ -6,6 +6,6 @@ tar -czf dist.tar.gz dist
 tar -czf webhook.tar.gz webhook
 echo "commit and push"
 git add .
-git commit -m "TOC aside layout"
+git commit -m "Add baidu tongji js code"
 git push origin main 
 echo "finish"
